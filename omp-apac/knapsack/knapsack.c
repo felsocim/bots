8b05ebdab884e44efb29a5743002af5b788f8b84
Marek FELŠÖCI <marek.felsoci@inria.fr>
2024-11-08 19:34:23 +0100

    [omp-apac] [knapsack] remove task directives

--- omp-apac/knapsack/knapsack.c
+++ omp-apac/knapsack/knapsack.c
@@ -36,8 +36,6 @@
 int best_so_far;
 int number_of_tasks;
 
-#pragma omp threadprivate(number_of_tasks)
-
 int compare(struct item *a, struct item *b)
 {
      double c = ((double) a->value / a->weight) -
@@ -79,7 +77,6 @@ int read_input(const char *filename, struct item *items, int *capacity, int *n)
  * return the optimal solution for n items (first is e) and
  * capacity c. Value so far is v.
  */
-#if defined(IF_CUTOFF)
 void knapsack_par(struct item *e, int c, int n, int v, int *sol, int l)
 {
      int with, without, best;
@@ -110,14 +107,11 @@ void knapsack_par(struct item *e, int c, int n, int v, int *sol, int l)
      /* 
       * compute the best solution without the current item in the knapsack 
       */
-     #pragma omp task untied firstprivate(e,c,n,v,l) shared(without) if (l < bots_cutoff_value)
      knapsack_par(e + 1, c, n - 1, v, &without,l+1);
 
      /* compute the best solution with the current item in the knapsack */
-     #pragma omp task untied firstprivate(e,c,n,v,l) shared(with)  if (l < bots_cutoff_value)
      knapsack_par(e + 1, c - e->weight, n - 1, v + e->value, &with,l+1);
 
-     #pragma omp taskwait
      best = with > without ? with : without;
 
      /* 
@@ -131,120 +125,7 @@ void knapsack_par(struct item *e, int c, int n, int v, int *sol, int l)
 
      *sol = best;
 }
-#elif defined (MANUAL_CUTOFF)
-void knapsack_par(struct item *e, int c, int n, int v, int *sol, int l)
-{
-     int with, without, best;
-     double ub;
 
-     number_of_tasks++;
-     /* base case: full knapsack or no items */
-     if (c < 0)
-     {
-         *sol = INT_MIN;
-         return;
-     }
-
-     /* feasible solution, with value v */
-     if (n == 0 || c == 0)
-     {
-         *sol = v;
-         return;
-     }
-
-     ub = (double) v + c * e->value / e->weight;
-
-     if (ub < best_so_far) {
-	  /* prune ! */
-          *sol = INT_MIN;
-          return;
-     }
-     if (l < bots_cutoff_value)
-     {
-        /* compute the best solution without the current item in the knapsack */
-        #pragma omp task untied firstprivate(e,c,n,v,l) shared(without)
-        knapsack_par(e + 1, c, n - 1, v, &without,l+1);
-
-        /* compute the best solution with the current item in the knapsack */
-        #pragma omp task untied firstprivate(e,c,n,v,l) shared(with)
-        knapsack_par(e + 1, c - e->weight, n - 1, v + e->value, &with,l+1);
-
-        #pragma omp taskwait
-     }
-     else
-     {
-        /* compute the best solution without the current item in the knapsack */
-        knapsack_seq(e + 1, c, n - 1, v, &without);
-        /* compute the best solution with the current item in the knapsack */
-        knapsack_seq(e + 1, c - e->weight, n - 1, v + e->value, &with);
-     }
-
-     best = with > without ? with : without;
-
-     /* 
-      * notice the race condition here. The program is still
-      * correct, in the sense that the best solution so far
-      * is at least best_so_far. Moreover best_so_far gets updated
-      * when returning, so eventually it should get the right
-      * value. The program is highly non-deterministic.
-      */
-     if (best > best_so_far) best_so_far = best;
-
-     *sol = best;
-}
-#else
-void knapsack_par(struct item *e, int c, int n, int v, int *sol, int l)
-{
-     int with, without, best;
-     double ub;
-
-     number_of_tasks++;
-     /* base case: full knapsack or no items */
-     if (c < 0)
-     {
-         *sol = INT_MIN;
-         return;
-     }
-
-     /* feasible solution, with value v */
-     if (n == 0 || c == 0)
-     {
-         *sol = v;
-         return;
-     }
-
-     ub = (double) v + c * e->value / e->weight;
-
-     if (ub < best_so_far) {
-	  /* prune ! */
-          *sol = INT_MIN;
-          return;
-     }
-     /* 
-      * compute the best solution without the current item in the knapsack 
-      */
-     #pragma omp task untied firstprivate(e,c,n,v,l) shared(without)
-     knapsack_par(e + 1, c, n - 1, v, &without,l+1);
-
-     /* compute the best solution with the current item in the knapsack */
-     #pragma omp task untied firstprivate(e,c,n,v,l) shared(with)
-     knapsack_par(e + 1, c - e->weight, n - 1, v + e->value, &with,l+1);
-
-     #pragma omp taskwait
-     best = with > without ? with : without;
-
-     /* 
-      * notice the race condition here. The program is still
-      * correct, in the sense that the best solution so far
-      * is at least best_so_far. Moreover best_so_far gets updated
-      * when returning, so eventually it should get the right
-      * value. The program is highly non-deterministic.
-      */
-     if (best > best_so_far) best_so_far = best;
-
-     *sol = best;
-}
-#endif
 void knapsack_seq(struct item *e, int c, int n, int v, int *sol)
 {
      int with, without, best;
@@ -296,19 +177,12 @@ void knapsack_seq(struct item *e, int c, int n, int v, int *sol)
 void knapsack_main_par (struct item *e, int c, int n, int *sol)
 {
      best_so_far = INT_MIN;
+     number_of_tasks = 0;
+     
+     knapsack_par(e, c, n, 0, sol, 0);
 
-     #pragma omp parallel
-     {
-        number_of_tasks = 0;
-        #pragma omp single
-        #pragma omp task untied
-        {
-           knapsack_par(e, c, n, 0, sol, 0);
-        }
-
-        #pragma omp critical
-        bots_number_of_tasks += number_of_tasks;
-     }
+     bots_number_of_tasks += number_of_tasks;
+     
      if (bots_verbose_mode) printf("Best value for parallel execution is %d\n\n", *sol);
 }
 void knapsack_main_seq (struct item *e, int c, int n, int *sol)

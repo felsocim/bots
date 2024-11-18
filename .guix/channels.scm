1647d7b4bd08d6dbf51f272e6650fc8531b78a5c
Marek FELŠÖCI <marek.felsoci@inria.fr>
2024-11-18 18:56:02 +0100

    Update software sources

--- .guix/channels.scm
+++ .guix/channels.scm
@@ -2,11 +2,11 @@
  (channel
   (name 'guix)
   (url "https://git.savannah.gnu.org/git/guix.git")
-  (commit "6d0a86d22d75f8f4bb602a35218513ae0aef71f1"))
+  (commit "1affd2b5aa7f5467a44cf757c4fc0c6956d3f3c9"))
  (channel
   (name 'guix-hpc)
   (url "https://gitlab.inria.fr/mfelsoci/guix-hpc.git")
-  (commit "3f0da0971807e12af0a397140593e16ebabc8e30"))
+  (commit "ed1b753bd766ec2d2deb0a80a682cfb5934e1b87"))
  (channel
   (name 'apac)
   (url "https://gitlab.inria.fr/apac/channel.git")

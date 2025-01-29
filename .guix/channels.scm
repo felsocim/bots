b02f18993733e96198e95acde558aa7de99399d1
Marek Felšöci <marek.felsoci@lip6.fr>
2025-01-29 16:24:19 +0100

    We do not need 'guix-hpc channel

--- .guix/channels.scm
+++ .guix/channels.scm
@@ -4,10 +4,6 @@
   (url "https://git.savannah.gnu.org/git/guix.git")
   (commit "1affd2b5aa7f5467a44cf757c4fc0c6956d3f3c9"))
  (channel
-  (name 'guix-hpc)
-  (url "https://gitlab.inria.fr/mfelsoci/guix-hpc.git")
-  (commit "ed1b753bd766ec2d2deb0a80a682cfb5934e1b87"))
- (channel
   (name 'apac)
   (url "https://gitlab.inria.fr/apac/channel.git")
   (commit "60acf2331487de20d043ce11b0ecf84b479e8685")))

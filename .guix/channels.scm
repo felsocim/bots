8d787cd0f4b4fb1de72f65631efb747ccbedee96
Marek Felšöci <marek.felsoci@lip6.fr>
2025-01-29 16:24:40 +0100

    Update 'guix-apac channel

--- .guix/channels.scm
+++ .guix/channels.scm
@@ -5,5 +5,5 @@
   (commit "1affd2b5aa7f5467a44cf757c4fc0c6956d3f3c9"))
  (channel
   (name 'apac)
-  (url "https://gitlab.inria.fr/apac/channel.git")
-  (commit "60acf2331487de20d043ce11b0ecf84b479e8685")))
+  (url "https://gitlab.com/apac-compiler/guix-apac.git")
+  (commit "156b2d522241611c57689535ed659f967a4a0359")))

5ae8ab057c546340a5c5c76bc71e7e1793a0397f
Marek Felšöci <marek.felsoci@lip6.fr>
2025-01-30 14:13:31 +0100

    [omp-apac] [concom] fix usage of atomic types

--- omp-apac/concom/atomic.cpp
+++ omp-apac/concom/atomic.cpp
@@ -1,10 +1,10 @@
-#include <stdatomic.h>
+#include <atomic>
 #include "atomic.hpp"
 
 int atomic_compare(int * v1, int * v2) {
-  return atomic_compare_exchange_strong((atomic_int *)v1, v2, 1);
+  return atomic_compare_exchange_strong((std::atomic_int *)v1, v2, 1);
 }
 
 void atomic_add(int * dest, int val) {
-  atomic_fetch_add((atomic_int *)dest, val);
+  atomic_fetch_add((std::atomic_int *)dest, val);
 }

ab3a9869e5559b7cb510871a86d378bfa9f37562
Marek Felšöci <marek.felsoci@lip6.fr>
2025-06-16 16:58:48 +0200

    Update software environment

--- .guix/channels.scm
+++ .guix/channels.scm
@@ -2,8 +2,14 @@
  (channel
   (name 'guix)
   (url "https://git.savannah.gnu.org/git/guix.git")
-  (commit "1affd2b5aa7f5467a44cf757c4fc0c6956d3f3c9"))
+  (branch "master")
+  (commit "6c9e010283424498ca094e163f29c1156df13752")
+  (introduction
+   (make-channel-introduction
+    "9edb3f66fd807b096b48283debdcddccfea34bad"
+    (openpgp-fingerprint
+     "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))
  (channel
   (name 'guix-apac)
   (url "https://gitlab.com/apac-compiler/guix-apac.git")
-  (commit "5e44dca7a0bf848ff606432600961279347cdfb9")))
+  (commit "aff044232f43a6e8e87d55cf93725506452a98cd")))

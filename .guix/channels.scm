c997fc1f27d0207e4576e05392d808fd7f37054b
Marek Felšöci <marek.felsoci@uvsq.fr>
2026-05-29 14:46:58 +0200

    Update software sources

--- .guix/channels.scm
+++ .guix/channels.scm
@@ -1,15 +1,14 @@
 (list
  (channel
   (name 'guix)
-  (url "https://git.savannah.gnu.org/git/guix.git")
+  (url "https://git.guix.gnu.org/guix.git")
   (branch "master")
-  (commit "6c9e010283424498ca094e163f29c1156df13752")
+  (commit "6d0a86d22d75f8f4bb602a35218513ae0aef71f1")
   (introduction
-   (make-channel-introduction
-    "9edb3f66fd807b096b48283debdcddccfea34bad"
+   (make-channel-introduction "9edb3f66fd807b096b48283debdcddccfea34bad"
     (openpgp-fingerprint
      "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))
  (channel
   (name 'guix-apac)
   (url "https://gitlab.com/apac-compiler/guix-apac.git")
-  (commit "aff044232f43a6e8e87d55cf93725506452a98cd")))
+  (commit "1e2e4f81bdba9827c9b0f05ebcbe41fcca67772f")))
